1d3dd2d89e5a79bd03354ba534916d178a2595c8
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-08-30 14:38:47 +0530

    Create __init__.py

---
+++ plugins/nimma/imdb2/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+API_ONE_URL = os.environ.get("IMDB_API_ONE_URL")
+API_TWO_URL = os.environ.get("IMDB_API_TWO_URL")
+API_THREE_URL = os.environ.get("IMDB_API_THREE_URL")
+WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "en_IN")
